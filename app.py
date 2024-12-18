d5b97b39043d99590a2f25711a9c6887c6b4a179
Charlie <cw8289502@gmail.com>
2024-12-18 10:22:47 +0800

    loginForm

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import db
 import flask
+from flask import render_template
 app = flask.Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5500, debug=True)
