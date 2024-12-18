5c45e54ac2a3cad5f6461391924a2b73cdaa29a1
Charlie <cw8289502@gmail.com>
2024-12-18 11:41:12 +0800

    loginAndLogout

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-import db
 import flask
+from flask import url_for
+from flask import session
+from flask import request
+from flask import redirect
 from flask import render_template
-app = flask.Flask(__name__)
 
-@app.route("/")
-def index():
+app = flask.Flask(__name__)
+app.secret_key = "NHES"
+
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        userId = request.values["userid"]
+        password = request.values["password"]
+        session["username"] = userId
+        return redirect(url_for("dashboard"))
+
     return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+@app.route("/dashboard")
+def dashboard():
+
+    return render_template("dashboard.html")
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5500, debug=True)
+    app.run(host="0.0.0.0", port=5800, debug=True)
